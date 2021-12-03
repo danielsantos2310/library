c69fc41fb3d7d55619aad32cd9465d822b1ff485
danielsantos2310 <71696181+danielsantos2310@users.noreply.github.com>
2021-12-03 15:09:33 +0000

    Add files via upload

---
+++ library/src/javax/swing/table.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javax.swing;
+
+/**
+ *
+ * @author Daniel Santos
+ */
+public class table {
+
+    public static class DefaultTableModel {
+
+        public DefaultTableModel() {
+        }
+
+        public void setcolumnIndentifiers(String[] columsName) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    public static class TableModel {
+
+        public TableModel() {
+        }
+
+        public void setcolumnIndentifiers(String[] columsName) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+}
